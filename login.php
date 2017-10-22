39723ae6f3d456e3972e3397cb0864b613174726
Jagai Berlinsky <jagaiberlinsky@gmail.com>
2017-10-22 18:15:17 -0300

    cookie logout

--- login.php
+++ login.php
@@ -12,8 +12,9 @@
 		$erroresFinales = validarLogin($_POST);
 		if (empty($erroresFinales)) {
 	      $usuario = comprobarEmail($_POST["email"]);
+		  $recordarme= $_POST["recordar"];
 	      loguear($usuario);
-	      cookiar($usuario);
+			($recordarme) ? cookiar($usuario):"";
 		  header('location: perfil-usuario.php'); exit;
 		}
 	}
@@ -26,13 +27,17 @@
 				<div class="contenedor-inputs">
 					<input type="email" name="email" placeholder="Correo" class="input-100"required>
 					<input type="password" name="pass" placeholder="Contraseña" class="input-100"required>
+					
 					<br>
 					<?php if (isset($erroresFinales['email'])): ?>
 						<span style="color: red;"><i class="ion-ios-close"></i></span>
 						<span style="color: red;"><?=$erroresFinales['email'];?></span>
 					<?php endif; ?>
 					<br><br>
-					<input type="submit" value="Entrar" class="btn-enviar">
+					<span class="input-48"><input type="radio" name="recordar" value="recordar">Recordarme</span>
+					<input type="submit" value="Entrar" class="btn-enviar" >
+			
+					
 				</form>
 				<?php if (isset($erroresFinales) && !empty($erroresFinales)): ?>
 					<div class="div-errores">
--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
  // Borra la cookie que almacena la sesión 
-  if(isset($_COOKIE[session_name()])) { 
-    setcookie(session_name(), '', time() - 42000, '/'); 
+  if(isset($_COOKIE['userid'])) { 
+    setcookie("userid", '', time() - 42000, '/'); 
   } 
   // Finalmente, destruye la sesión 
 	session_destroy();

0f4f89b1817993ebbe352045f225ebac509a9cdf
Alexis <alexis.knack@hotmail.com>
2017-10-23 16:44:36 -0300

    arreglos MRK 2

--- perfil-usuario.php
+++ perfil-usuario.php
@@ -4,13 +4,10 @@
 	if(!estaLogueado()){
 		header('Location: login.php'); exit;
 	}
-		if (estaCookiado()) {
-		header('Location: index.php'); exit;
-		}
 	require_once('includes/head.php');
+	$tituloDePagina = 'Perfil del Usuario';
 	$elUsuario = traerId($_SESSION['userId']);
 	$laImagen = glob('images/avatares/' . $elUsuario['email'] . '*');
-	$tituloDePagina = 'Perfil del Usuario';
 ?>
 		<h2>Hola <?=$elUsuario['name'];?> <?=$elUsuario['lastname'];?></h2>
 		<em><?=$elUsuario['email'];?></em>
@@ -18,7 +15,6 @@
 		<img src="<?=$laImagen[0];?>" width="100" alt="avatar" style="border-radius: 50%;">
 		<br><br>
 		<a href="index.php" class="button">Volver al inicio</a>
-		<a href="modificar.php" class="button">Modificar datos</a>
 		<a href="logout.php" class="button">Salir</a>
 	</body>
 </html>

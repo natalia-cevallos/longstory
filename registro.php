46c7874a1fc147ef1ca65f748d3dcff97a6d2e70
Ezpz <hugosajama07@gmail.com>
2017-10-23 16:51:09 -0300

    error merge

--- registro.php
+++ registro.php
@@ -41,44 +41,34 @@
 		<form method="post" class="form-register" enctype="multipart/form-data">
 				<h2 class="form-titulo"> CREA UNA CUENTA </h2>
 				<div class="contenedor-inputs">
-					<input type="text" name="name" value="<?=$nombre;?>" placeholder="ej. Juan" class="input-48" required>
+					<input type="text" name="name" value="<?=$nombre;?>" placeholder="Nombre" class="input-48" required>
 					<?php if (isset($erroresFinales['nombre'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['nombre'];?></span>
+						<span class="error"><?=$erroresFinales['nombre'];?></span>
 					<?php endif; ?>
 
-					<input type="text" name="apellido" value="<?=$apellido;?>" placeholder="ej. Gomez" class="input-48"required>
+					<input type="text" name="apellido" value="<?=$apellido;?>" placeholder="Apellido" class="input-48"required>
 					<?php if (isset($erroresFinales['apellido'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['apellido'];?></span>
+						<span class="error"><?=$erroresFinales['apellido'];?></span>
 					<?php endif; ?>
 
-					<input type="text" name="email" value="<?=$email;?>" placeholder="ej. juang@.tucorreo.com" class="input-100"required>
+					<input type="text" name="email" value="<?=$email;?>" placeholder="Correo Electronico" class="input-100"required>
 					<?php if (isset($erroresFinales['email'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['email'];?></span>
+						<span class="error"><?=$erroresFinales['email'];?></span>
 					<?php endif; ?>
 
-					<input type="text" name="username" value="<?=$username;?>" placeholder="username" class="input-48"required>
+					<input type="text" name="username" value="<?=$username;?>" placeholder="Usiario" class="input-100"required>
 					<?php if (isset($erroresFinales['username'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['username'];?></span>
+						<span class="error"><?=$erroresFinales['username'];?></span>
 					<?php endif; ?>
 
-					<input type="text" name="telefono" value="<?=$telefono;?>" placeholder="Telefono" class="input-48"required>
-					<?php if (isset($erroresFinales['telefono'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['telefono'];?></span>
-					<?php endif; ?>
+
 					<input type="password" name="pass" placeholder="Contraseña" class="input-100"required>
 					<?php if (isset($erroresFinales['pass'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['pass'];?></span>
+						<span class="error"><?=$erroresFinales['pass'];?></span>
 					<?php endif; ?>
 					<input type="password" name="repass" placeholder="Repetir Contraseña" class="input-100"required>
 					<?php if (isset($erroresFinales['repass'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['repass'];?></span>
+						<span class="error"><?=$erroresFinales['repass'];?></span>
 					<?php endif; ?>
 					<select class="input-48" name="sexo">
 						<option value="">Indicar Sexo</option>
@@ -91,29 +81,20 @@
 						<?php endforeach; ?>
 					</select>
 					<?php if (isset($erroresFinales['sexo'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['sexo'];?></span>
+						<span class="error"><?=$erroresFinales['sexo'];?></span>
 					<?php endif; ?>
 					<br>
 					<input type="file" name="avatar" multiple class="input-48">
 					<?php if (isset($erroresFinales['imagen'])): ?>
-						<span style="color: red;"><i class="ion-ios-close"></i></span>
-						<span style="color: red;"><?=$erroresFinales['imagen'];?></span>
+						<span class="error"><?=$erroresFinales['imagen'];?></span>
 					<?php endif; ?>
 					<input type="submit" value="Registrar" class="btn-enviar">
 					<p class="form-link"> ¿Ya tienes una cuenta? <a href="login.php"> Ingresa aquí </a></p>
 				</div>
 		</form>
 <br><br><br><br><br><br><br><br><br><br>
-		<?php if (isset($erroresFinales) && !empty($erroresFinales)): ?>
-			<div class="div-errores">
-				<ul>
-					<?php foreach ($erroresFinales as $clave => $error): ?>
-						<li> <?=$error?> </li>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-		<?php endif; ?>
+
+
 <?php
 	include_once ("includes/end.php");
 ?>

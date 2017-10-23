c27f14330b0cbb10ebc51e510c94199d0e34db8d
Alexis <alexis.knack@hotmail.com>
2017-10-23 17:35:25 -0300

    cosas

--- perfil-usuario.php
+++ perfil-usuario.php
@@ -1,14 +1,31 @@
 <?php
 	session_start();
+
 	require_once('funciones.php');
+
 	if(!estaLogueado()){
 		header('Location: login.php'); exit;
 	}
+
+	if (estaCookiado()) {
+		$cookie = $_COOKIE['userid'];
+		$usuario = traerId($cookie);
+		loguear($usuario);
+	}
+
+	if (estaLogueado()) {
+		$usuario = traerId($_SESSION['userId']);
+		$laImagen = glob('images/avatares/' . $usuario['email'] . '*');
+	}
+
+
 	$tituloDePagina = 'Perfil del Usuario';
 	require_once('includes/head.php');
-	$elUsuario = traerId($_SESSION['userId']);
-	$laImagen = glob('images/avatares/' . $elUsuario['email'] . '*');
 ?>
+<?php
+$elUsuario = traerId($_SESSION['userId']);
+$laImagen = glob('images/avatares/' . $elUsuario['email'] . '*');
+ ?>
 	<img src="<?=$laImagen[0];?>" width="250" alt="avatar" style="border-radius: 50%;">
 		<h2>Hola <?=$elUsuario['name'];?> <?=$elUsuario['lastname'];?></h2>
 		<em><?=$elUsuario['email'];?></em>
